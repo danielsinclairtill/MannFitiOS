733647d54f8081c4e9ab8a8d785e5a093ad83b27
Daniel Till <danieltill@admins-MacBook-Air-10.local>
2017-11-16 14:10:29 -0500

    Pacman Smooth Path P3

--- MannFit/GameScene.swift
+++ MannFit/GameScene.swift
@@ -65,7 +65,7 @@ class GameScene: SKScene {
         scoreLabel.zPosition = 1
         scoreLabel.fontName = "AvenirNextCondensed-Heavy"
         scoreLabel.fontSize = 50.0
-        scoreLabel.fontColor = SKColor.white
+        scoreLabel.fontColor = SKColor.darkGray
         var scoreText = String(score)
         scoreLabel.text = scoreText
         scoreLabel.horizontalAlignmentMode = .right
