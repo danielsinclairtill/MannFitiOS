da7a4d03dfaa847a32383a05c22c4742b3ebe1eb
Daniel Till <danieltill@Daniels-MacBook-Air.local>
2017-12-27 15:02:38 -0500

    4D_SettingsTab

---
+++ MannFit/Tab Bar/SettingsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.swift
+//  MannFit
+//
+//  Created by Daniel Till on 12/27/17.
+//  Copyright © 2017 MannFit Labs. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+    
+}
