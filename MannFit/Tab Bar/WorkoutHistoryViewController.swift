52c569bb0e27fcbdf51e6d7ed7b1cb5536d93bd8
Luis Abraham <luisabraham_11@hotmail.com>
2018-01-02 18:26:08 -0500

    Implemented FetchedResultsController to fetch workout data and popula… (#19)
    * Implemented FetchedResultsController to fetch workout data and populate workout history page.
    
    * Removing project settings change

--- MannFit/Tab Bar/WorkoutHistoryViewController.swift
+++ MannFit/Tab Bar/WorkoutHistoryViewController.swift
@@ -11,30 +11,32 @@ import CoreData
 
 class WorkoutHistoryViewController: UITableViewController {
     
-    struct DummyData {
-        var score: Double
-        var name: String
-        var date: Date
-    }
-    
     var managedObjectContext: NSManagedObjectContext!
     
-    var data: [DummyData] = {
-        let item1 = DummyData(score: 4.65, name: "PacMan", date: Date())
-        let item2 = DummyData(score: 6.16, name: "PacMan", date: Date())
-        let item3 = DummyData(score: 7.21, name: "PacMan", date: Date())
-        return [item1, item2, item3]
+    private lazy var fetchedResultsController: NSFetchedResultsController<WorkoutItem> = {
+        let fetchRequest: NSFetchRequest<WorkoutItem> = WorkoutItem.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+        
+        return NSFetchedResultsController(fetchRequest: fetchRequest,
+                                          managedObjectContext: self.managedObjectContext,
+                                          sectionNameKeyPath: nil,
+                                          cacheName: nil)
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.black
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.view.backgroundColor = UIColor.black
+       
+        do {
+            try self.fetchedResultsController.performFetch()
+        } catch {
+            // TODO: Handle the error cleanly
+            print(error)
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -51,20 +53,15 @@ class WorkoutHistoryViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return self.fetchedResultsController.fetchedObjects?.count ?? 0
     }
     
-    // This will all be cleaned up in another commit
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Storyboard.WorkoutCell, for: indexPath) as! WorkoutTableViewCell
         
-        let item = data[indexPath.row]
-        cell.workoutNameLabel.text = item.name
-        cell.workoutScoreLabel.text = "\(String(format: "%.2f", item.score))"
-        cell.workoutImageView.image = UIImage(named: item.name)
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        cell.workoutDateLabel.text = dateFormatter.string(from: item.date)
+        let item = self.fetchedResultsController.object(at: indexPath)
+        
+        cell.configureCell(workoutItem: item)
         
         return cell
     }
