4de592d690c7592b28938516b47553d4913123e6
Daniel Till <daniel.sinclair.till@gmail.com>
2018-01-05 15:49:48 -0500

    11D_GameOverPrompPopUp (#22)

--- MannFit/Game/PacManGameViewController.swift
+++ MannFit/Game/PacManGameViewController.swift
@@ -13,27 +13,55 @@ import CoreData
 class PacManGameViewController: UIViewController, CoreDataCompliant {
     
     var managedObjectContext: NSManagedObjectContext!
+    private var scene: PacManGameScene?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setNeedsStatusBarAppearanceUpdate()
-        let scene = PacManGameScene(size: view.bounds.size)
-        scene.gameOverDelegate = self
+        self.scene = PacManGameScene(size: view.bounds.size)
+        scene?.gameOverDelegate = self
         UIApplication.shared.isIdleTimerDisabled = true
         let skView = view as! SKView
         skView.showsFPS = true
         skView.showsNodeCount = true
         skView.ignoresSiblingOrder = true
-        scene.scaleMode = .resizeFill
+        scene?.scaleMode = .resizeFill
         skView.presentScene(scene)
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    private func exit() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PacManGameViewController: GameOverPromptDelegate {
+    func restartGame() {
+        // first dismiss popup
+        self.dismiss(animated: true, completion: nil)
+        self.scene?.restartGame()
+    }
+    
+    func exitGame() {
+        // first dismiss popup
+        self.dismiss(animated: true, completion: nil)
+        self.scene?.engine?.stop()
+        self.exit()
+    }
 }
 
 extension PacManGameViewController: GameOverDelegate {
+    
+    func presentPrompt() {
+        let view = GameOverPromptView()
+        view.delegate = self
+        let popup = PopUpViewController(view: view, dismissible: false)
+        self.present(popup, animated: true, completion: nil)
+    }
+    
     func sendGameData(game: String, duration: Int, absement: Float) {
         self.prepareItem(game: game, duration: duration, absement: absement)
         self.managedObjectContext.saveChanges()
@@ -46,9 +74,5 @@ extension PacManGameViewController: GameOverDelegate {
         workoutItem.absement = absement
         workoutItem.date = Date()
         workoutItem.caloriesBurned = 0 // calculate this after
-    }
-    
-    func exitGame() {
-        self.dismiss(animated: true, completion: nil)
     }
 }
