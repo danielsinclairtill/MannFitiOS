7e25f7889f9278a0da9b072a9eac6828e812da11
Daniel Till <danieltill@Daniels-MacBook-Air.local>
2018-04-02 13:53:03 -0400

    D_GraphSettings

--- MannFit/Tab Bar/WorkoutDetailViewController.swift
+++ MannFit/Tab Bar/WorkoutDetailViewController.swift
@@ -20,7 +20,7 @@ class WorkoutDetailViewController: UIViewController {
     @IBOutlet weak var improvementLabel: UILabel!
     @IBOutlet weak var absementGraphLabel: UILabel!
     @IBOutlet weak var absementGraphView: AbsementGraphView!
-        
+
     var workoutName: String!
     var absementScore: String!
     var date: Date!
